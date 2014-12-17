f93c2180d037d5c665df0bf67ebb0c452b3c6adb
Marco Davids <marco.davids@sidn.nl>
2014-12-17 14:40:21 +0100

    dkimcheck_new.pl
    Some experiments with a new, hopefully miproved version.
    Not finished yet.

---
+++ dkimcheck_new.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl -wT
+#
+# Work in progess!
+# SIDN Labs (Marco)
+# 
+use strict;
+use warnings;
+
+use Mail::DKIM::Verifier;
+
+# create a verifier object
+my $dkim = Mail::DKIM::Verifier->new();
+
+# or read an email and pass it into the verifier, incrementally
+while (<STDIN>)
+{
+# remove local line terminators
+chomp;
+s/\015$//;
+
+# use SMTP line terminators
+$dkim->PRINT("$_\015\012");
+}
+
+$dkim->CLOSE;
+
+# what is the result of the verify?
+my $result = $dkim->result;
+my $detail = $dkim->result_detail;
+print "Result: " . $result . "\n";
+print "Detail: " . $detail . "\n";
+
+# there might be multiple signatures, what is the result per signature?
+foreach my $signature ($dkim->signatures)
+{
+print "signature identity: " . $signature->identity . "\n";
+print "verify result: " . $signature->result_detail . "\n";
+}
+
+# the alleged author of the email may specify how to handle email
+foreach my $policy ($dkim->policies)
+{
+die "fraudulent message" if ($policy->apply($dkim) eq "reject");
+}
