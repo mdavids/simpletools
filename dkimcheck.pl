2abc5d842522d42d79523e10f57d10d0c5e35f15
Marco Davids <marco.davids@sidn.nl>
2014-12-17 14:38:01 +0100

    dkimcheck.pl
    Simple Perl based DKIM-checker (for debugging purposes)

---
+++ dkimcheck.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl -wT
+#
+# Author: SIDN Labs (Marco Davids)
+# Usage: ./dkimcheck.pl < ./test.eml
+#
+use strict;
+
+# verify a message
+use Mail::DKIM::Verifier;
+
+# create a verifier object
+my $dkim = Mail::DKIM::Verifier->new();
+
+# read an email from stdin, pass it into the verifier
+while (<STDIN>)
+{
+ # remove local line terminators
+ chomp;
+ s/\015$//;
+
+ # use SMTP line terminators
+ $dkim->PRINT("$_\015\012");
+}
+$dkim->CLOSE;
+
+# what is the result of the verify?
+my $result = $dkim->result;
+my $detail = $dkim->result_detail;
+
+print "Result: " . $result . "\n";
+print "Detail: " . $detail . "\n";
