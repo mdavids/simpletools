a7f71fcdd0ac312c90b6896513806a65ac30ef88
Marco Davids <marco.davids@sidn.nl>
2016-03-09 09:57:20 +0100

    Remove the first line, instead of first- and last.

--- nlretro.sh
+++ nlretro.sh
@@ -14,12 +14,12 @@ fi
 if [ "$2" == "-d" ]
 then
 	# Sort with most recent date first
-	echo $1 | nc retro.domain-registry.nl 43 | sed '1d;$d' |  awk -F\; '{print $2";"$1}' | sort  -t"-" -rk 3 -rk 2 -rk 1 | awk -F\; '{print $2";"$1}'
+	echo $1 | nc retro.domain-registry.nl 43 | sed '1d' |  awk -F\; '{print $2";"$1}' | sort  -t"-" -rk 3 -rk 2 -rk 1 | awk -F\; '{print $2";"$1}'
 else
 	if [ "$2" == "-l" ]
 	then
 		# Sort with shortest name first
-		echo $1 | nc retro.domain-registry.nl 43 | sed '1d;$d' |  awk '{ print length, $0 }' | sort -n | cut -d" " -f2-
+		echo $1 | nc retro.domain-registry.nl 43 | sed '1d' |  awk '{ print length, $0 }' | sort -n | cut -d" " -f2-
 	else
 		if [ "$2" != "" ]
 		then
@@ -28,7 +28,7 @@ else
 		else
 			# Default behaviour
 			# As with the rest; skip the first line, which is a counter
-			echo $1 | nc retro.domain-registry.nl 43 | sed '1d;$d'
+			echo $1 | nc retro.domain-registry.nl 43 | sed '1d'
 		fi
 	fi
 fi
